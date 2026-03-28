33f367db2b8a8beceef4ba31d5247654767040a6
Victor Fongaro <165833874+victorfongaro@users.noreply.github.com>
2026-03-28 11:57:22 -0300

    Struture

---
+++ script.js
@@ -0,0 +1,10 @@
+const button1 = document.getElementById('button-1');
+const button2 = document.getElementById('button-2');
+
+button1.addEventListener('click', function() {
+    window.open('projetos.html', '_blank');
+});
+
+button2.addEventListener('click', function() {
+    window.open('resumo.html', '_blank');
+});
